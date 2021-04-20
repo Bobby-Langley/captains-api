2794c55872813b81439b56cf53f4b34871308671
Bobby Langley <bobbylangley227@gmail.com>
2021-04-19 21:06:50 -0400

    full crud enabled, api points made

--- functions/index.js
+++ functions/index.js
@@ -2,19 +2,19 @@ const functions = require("firebase-functions");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const { getUsers, getSingleUser, postUser, updateUser, deleteUser} = require("./src/users/index")
-
 const app = express();
+
 app.use(bodyParser.json());
 app.use(cors());
 
+const { getUsers, getSingleUser, postUser, updateUser, deleteUser} = require("./src/users/index")
+
 app.get("/users", getUsers);
+app.get("/user/:userId", getSingleUser);
+
 app.post("/users", postUser);
 app.patch("/users/:userId", updateUser);
 app.delete("/users/:userId", deleteUser);
-
-
-app.get("/user/:userId", getSingleUser);
 
 
 exports.app = functions.https.onRequest(app);
